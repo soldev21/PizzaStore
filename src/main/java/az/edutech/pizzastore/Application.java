6da0e0788fa22ec4482bb92ccca690aab806ee54
soldev21 <sherif.huseynli@gmail.com>
2020-04-26 14:12:30 +0400

    initial

--- src/main/java/az/edutech/pizzastore/Application.java
+++ src/main/java/az/edutech/pizzastore/Application.java
@@ -1,7 +1,6 @@
 package az.edutech.pizzastore;
 
 import az.edutech.pizzastore.model.Cheque;
-import az.edutech.pizzastore.store.PizzaStore;
 
 public class Application {
     public static void main(String[] args) {
